06a4e696cf95a8e57b28b277d52ce3c6e20fe2a6
grimes2 <juergen.lange@unitybox.de>
2023-04-30 14:26:44 +0200

    Sort

--- foo_preview.cpp
+++ foo_preview.cpp
@@ -46,70 +46,71 @@ static const GUID guid_cfg_branch =
 { 0x90073616, 0x61a0, 0x473d, { 0xa1, 0x72, 0x70, 0x39, 0x24, 0xfe, 0xb0, 0xa1 } };
 static advconfig_branch_factory cfg_branch("Preview", guid_cfg_branch, advconfig_entry::guid_branch_playback, 0);
 
-// {43009833-D765-4515-9C59-7EED782B622B}
-static const GUID guid_cfg_preview_length =
-{ 0x43009833, 0xd765, 0x4515, { 0x9c, 0x59, 0x7e, 0xed, 0x78, 0x2b, 0x62, 0x2b } };
-advconfig_string_factory cfg_preview_length("Preview length (s)", guid_cfg_preview_length, guid_cfg_branch, 0, "5");
-
-// {547E2382-09CB-4EC3-B325-C0FFBFF3BDA6}
-static const GUID guid_cfg_start_time_seconds =
-{ 0x547e2382, 0x9cb, 0x4ec3, { 0xb3, 0x25, 0xc0, 0xff, 0xbf, 0xf3, 0xbd, 0xa6 } };
-advconfig_string_factory cfg_start_time_seconds("Preview start (s)", guid_cfg_start_time_seconds, guid_cfg_branch, 0, "0");
-
-// {7C8B7E19-5BA9-4391-9299-20CCC620F4E7}
-static const GUID guid_cfg_start_time_percent_enabled =
-{ 0x7c8b7e19, 0x5ba9, 0x4391, { 0x92, 0x99, 0x20, 0xcc, 0xc6, 0x20, 0xf4, 0xe7 } };
-advconfig_checkbox_factory cfg_start_time_percent_enabled("Preview start in %", guid_cfg_start_time_percent_enabled, guid_cfg_branch, 0, false);
-
-// {E0B5AA2A-189E-4F1C-B895-6720B22FA4EA}
-static const GUID guid_cfg_preview_start_random_enabled =
-{ 0xe0b5aa2a, 0x189e, 0x4f1c, { 0xb8, 0x95, 0x67, 0x20, 0xb2, 0x2f, 0xa4, 0xea } };
-advconfig_checkbox_factory cfg_preview_start_random_enabled("Preview start random", guid_cfg_preview_start_random_enabled, guid_cfg_branch, 0, false);
-
-// {1D5D5C64-18E6-4FF5-B5DE-50CEDA4E975D}
-static const GUID guid_cfg_start_time_percent =
-{ 0x1d5d5c64, 0x18e6, 0x4ff5, { 0xb5, 0xde, 0x50, 0xce, 0xda, 0x4e, 0x97, 0x5d } };
-advconfig_string_factory cfg_start_time_percent("Preview start (%)", guid_cfg_start_time_percent, guid_cfg_branch, 0, "50");
-
-// {91876C5A-7200-4FCC-BAAE-1B77F1D48881}
-static const GUID guid_cfg_track_length_bypass =
-{ 0x91876c5a, 0x7200, 0x4fcc, { 0xba, 0xae, 0x1b, 0x77, 0xf1, 0xd4, 0x88, 0x81 } };
-advconfig_string_factory cfg_track_length_bypass("Track length bypass (s)", guid_cfg_track_length_bypass, guid_cfg_branch, 0, "0");
-
-// {713B159B-E2D4-4D5C-96C0-7B172C32E22B}
-static const GUID guid_cfg_preview_length_percent_enabled =
-{ 0x713b159b, 0xe2d4, 0x4d5c, { 0x96, 0xc0, 0x7b, 0x17, 0x2c, 0x32, 0xe2, 0x2b } };
-advconfig_checkbox_factory cfg_preview_length_percent_enabled("Preview length in %", guid_cfg_preview_length_percent_enabled, guid_cfg_branch, 0, false);
-
-// {C48ABAAC-EF39-43A7-B34E-88A7BDB6F579}
-static const GUID guid_cfg_preview_length_percent =
-{ 0xc48abaac, 0xef39, 0x43a7, { 0xb3, 0x4e, 0x88, 0xa7, 0xbd, 0xb6, 0xf5, 0x79 } };
-advconfig_string_factory cfg_preview_length_percent("Preview length (%)", guid_cfg_preview_length_percent, guid_cfg_branch, 0, "5");
-
-// {A875DA0F-BE63-453F-A5F5-9E507A68D61F}
-static const GUID guid_cfg_total_length_percent_enabled =
-{ 0xa875da0f, 0xbe63, 0x453f, { 0xa5, 0xf5, 0x9e, 0x50, 0x7a, 0x68, 0xd6, 0x1f } };
-advconfig_checkbox_factory cfg_total_length_percent_enabled("Preview end in %", guid_cfg_total_length_percent_enabled, guid_cfg_branch, 0, false);
-
-// {A86FD538-7B93-43B9-991D-2F5FDACC3A1A}
-static const GUID guid_cfg_total_length_percent =
-{ 0xa86fd538, 0x7b93, 0x43b9, { 0x99, 0x1d, 0x2f, 0x5f, 0xda, 0xcc, 0x3a, 0x1a } };
-advconfig_string_factory cfg_total_length_percent("Preview end (%)", guid_cfg_total_length_percent, guid_cfg_branch, 0, "50");
-
 // {1787F975-F3DF-410E-AAB1-954D8F7A2C41}
 static const GUID guid_cfg_loop_enabled =
 { 0x1787f975, 0xf3df, 0x410e, { 0xaa, 0xb1, 0x95, 0x4d, 0x8f, 0x7a, 0x2c, 0x41 } };
 advconfig_checkbox_factory cfg_loop_enabled("Loop", guid_cfg_loop_enabled, guid_cfg_branch, 0, false);
 
-// {585C9B33-36A6-4FBC-98D6-11326A2B4982}
-static const GUID guid_cfg_preview_length_limit =
-{ 0x585c9b33, 0x36a6, 0x4fbc, { 0x98, 0xd6, 0x11, 0x32, 0x6a, 0x2b, 0x49, 0x82 } };
-advconfig_string_factory cfg_preview_length_limit("Preview length limit (s)", guid_cfg_preview_length_limit, guid_cfg_branch, 0, "30");
+// {43009833-D765-4515-9C59-7EED782B622B}
+static const GUID guid_cfg_preview_length =
+{ 0x43009833, 0xd765, 0x4515, { 0x9c, 0x59, 0x7e, 0xed, 0x78, 0x2b, 0x62, 0x2b } };
+advconfig_string_factory cfg_preview_length("Preview length (s)", guid_cfg_preview_length, guid_cfg_branch, 0, "10");
+
+// {C48ABAAC-EF39-43A7-B34E-88A7BDB6F579}
+static const GUID guid_cfg_preview_length_percent =
+{ 0xc48abaac, 0xef39, 0x43a7, { 0xb3, 0x4e, 0x88, 0xa7, 0xbd, 0xb6, 0xf5, 0x79 } };
+advconfig_string_factory cfg_preview_length_percent("Preview length (%)", guid_cfg_preview_length_percent, guid_cfg_branch, 1, "5");
+
+// {713B159B-E2D4-4D5C-96C0-7B172C32E22B}
+static const GUID guid_cfg_preview_length_percent_enabled =
+{ 0x713b159b, 0xe2d4, 0x4d5c, { 0x96, 0xc0, 0x7b, 0x17, 0x2c, 0x32, 0xe2, 0x2b } };
+advconfig_checkbox_factory cfg_preview_length_percent_enabled("Preview length in %", guid_cfg_preview_length_percent_enabled, guid_cfg_branch, 1, false);
 
 // {834875C8-C519-4468-8E51-32E5787560B6}
 static const GUID guid_cfg_preview_length_random_enabled =
 { 0x834875c8, 0xc519, 0x4468, { 0x8e, 0x51, 0x32, 0xe5, 0x78, 0x75, 0x60, 0xb6 } };
-advconfig_checkbox_factory cfg_preview_length_random_enabled("Preview length random", guid_cfg_preview_length_random_enabled, guid_cfg_branch, 0, false);
+advconfig_checkbox_factory cfg_preview_length_random_enabled("Preview length random", guid_cfg_preview_length_random_enabled, guid_cfg_branch, 1, false);
+
+// {547E2382-09CB-4EC3-B325-C0FFBFF3BDA6}
+static const GUID guid_cfg_start_time_seconds =
+{ 0x547e2382, 0x9cb, 0x4ec3, { 0xb3, 0x25, 0xc0, 0xff, 0xbf, 0xf3, 0xbd, 0xa6 } };
+advconfig_string_factory cfg_start_time_seconds("Preview start (s)", guid_cfg_start_time_seconds, guid_cfg_branch, 1, "0");
+
+// {1D5D5C64-18E6-4FF5-B5DE-50CEDA4E975D}
+static const GUID guid_cfg_start_time_percent =
+{ 0x1d5d5c64, 0x18e6, 0x4ff5, { 0xb5, 0xde, 0x50, 0xce, 0xda, 0x4e, 0x97, 0x5d } };
+advconfig_string_factory cfg_start_time_percent("Preview start (%)", guid_cfg_start_time_percent, guid_cfg_branch, 2, "50");
+
+// {7C8B7E19-5BA9-4391-9299-20CCC620F4E7}
+static const GUID guid_cfg_start_time_percent_enabled =
+{ 0x7c8b7e19, 0x5ba9, 0x4391, { 0x92, 0x99, 0x20, 0xcc, 0xc6, 0x20, 0xf4, 0xe7 } };
+advconfig_checkbox_factory cfg_start_time_percent_enabled("Preview start in %", guid_cfg_start_time_percent_enabled, guid_cfg_branch, 3, false);
+
+// {E0B5AA2A-189E-4F1C-B895-6720B22FA4EA}
+static const GUID guid_cfg_preview_start_random_enabled =
+{ 0xe0b5aa2a, 0x189e, 0x4f1c, { 0xb8, 0x95, 0x67, 0x20, 0xb2, 0x2f, 0xa4, 0xea } };
+advconfig_checkbox_factory cfg_preview_start_random_enabled("Preview start random", guid_cfg_preview_start_random_enabled, guid_cfg_branch, 3, false);
+
+// {A875DA0F-BE63-453F-A5F5-9E507A68D61F}
+static const GUID guid_cfg_total_length_percent_enabled =
+{ 0xa875da0f, 0xbe63, 0x453f, { 0xa5, 0xf5, 0x9e, 0x50, 0x7a, 0x68, 0xd6, 0x1f } };
+advconfig_checkbox_factory cfg_total_length_percent_enabled("Preview end in %", guid_cfg_total_length_percent_enabled, guid_cfg_branch, 4, false);
+
+// {A86FD538-7B93-43B9-991D-2F5FDACC3A1A}
+static const GUID guid_cfg_total_length_percent =
+{ 0xa86fd538, 0x7b93, 0x43b9, { 0x99, 0x1d, 0x2f, 0x5f, 0xda, 0xcc, 0x3a, 0x1a } };
+advconfig_string_factory cfg_total_length_percent("Preview end (%)", guid_cfg_total_length_percent, guid_cfg_branch, 4, "55");
+
+// {585C9B33-36A6-4FBC-98D6-11326A2B4982}
+static const GUID guid_cfg_preview_length_limit =
+{ 0x585c9b33, 0x36a6, 0x4fbc, { 0x98, 0xd6, 0x11, 0x32, 0x6a, 0x2b, 0x49, 0x82 } };
+advconfig_string_factory cfg_preview_length_limit("Preview length limit (s)", guid_cfg_preview_length_limit, guid_cfg_branch, 5, "30");
+
+// {91876C5A-7200-4FCC-BAAE-1B77F1D48881}
+static const GUID guid_cfg_track_length_bypass =
+{ 0x91876c5a, 0x7200, 0x4fcc, { 0xba, 0xae, 0x1b, 0x77, 0xf1, 0xd4, 0x88, 0x81 } };
+advconfig_string_factory cfg_track_length_bypass("Track length bypass (s)", guid_cfg_track_length_bypass, guid_cfg_branch, 6, "0");
+
 
 VOID CALLBACK PreviewTimer(
 	HWND,        // handle to window for timer messages
